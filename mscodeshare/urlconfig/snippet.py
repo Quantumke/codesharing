5ce71ba128ab16a9b7e420b61ec802a3e0713301
GodMode <nguruben@gmail.com>
2016-05-10 12:02:30 +0300

    regex error resolved

--- mscodeshare/urlconfig/snippet.py
+++ mscodeshare/urlconfig/snippet.py
@@ -1,4 +1,5 @@
-from django.views.generic.list_detail import object_list, object_detail
+from django.views.generic.list import  ListView
+from django.views.generic.detail import DetailView
 from codeshare.models import  snippet
 from django.conf.urls import include, url
 from django.conf.urls import patterns, include, url
@@ -8,8 +9,8 @@ from django.conf.urls import patterns, include, url
 
 snippet_info= {'query': snippet.objects.all()}
 urlpatterns = patterns('',
-                       url(r'^$',object_list, dict(snippet_info, paginate_by=20),
+                       url(r'^$',ListView.as_view(), dict(snippet_info, paginate_by=20),
                            name='snippet_info'),
-                       url(r'^(?P<object_id>\d+)/$',object_detail, snippet_info,
-                           name="snippet_details"),
+                       url(r'^(?P<object_id>\d+)/$',DetailView.as_view(), snippet_info,
+                           name='snippet_details'),
                        )
--- mscodeshare/urls.py
+++ mscodeshare/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    (r'^snippets/', include('mscodeshare.urlconfig.snippet')),
+    url(r'^snippets/', include('mscodeshare.urlconfig.snippet')),
 ]
